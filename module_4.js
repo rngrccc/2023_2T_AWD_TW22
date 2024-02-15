e260e466b6b2236f1f2d844eb126f88fd09d3df9
rngrccc <155609702+rngrccc@users.noreply.github.com>
2024-02-15 21:01:35 +0800

    Add files via upload

---
+++ module_4.js
@@ -0,0 +1,12 @@
+function doMath(wew){
+    var result = wew;
+    document.form1.answer.value += result;
+}
+    
+function equal (){
+    form1.answer.value = eval(form1.answer.value);
+}
+    
+function clear1 (wewes){
+    var output = document.getElementById("display").value = "";
+}
